34c31866b37e54bcb05285577c7b87d883d51b0c
sefa mankaloglu <sefa78.m@hotmail.com>
2023-04-05 19:35:15 +0100

    Update

--- tesst/main.cpp
+++ tesst/main.cpp
@@ -2,8 +2,24 @@
 
 int main()
 {
-    ServerStartup();
-    threadPool thpool(std::thread::hardware_concurrency());
+    int Num;
+    std::cout << "Enter Thread Count, Enter 0 for supported Max threads" << std::endl;
+    std::cin >> Num;
+
+    if (ServerStartup())
+    {
+        if(Num==0 || Num > std::thread::hardware_concurrency())
+			threadPool thpool(std::thread::hardware_concurrency());
+        else
+        {
+            threadPool thpool(Num);
+        }
+    }
+    else
+    {
+        exit(0);
+        system("Pause");
+    }
     ServerShutdown();
     system("Echo Server Shutdown Successful");
 }
